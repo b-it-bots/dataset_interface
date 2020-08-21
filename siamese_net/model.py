7d9a8c519e7ceffe0511c5ca7e675479bb9a4226
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-08-21 10:59:39 +0200

    [siamese_net] Used colour images and increased embedding size
    While testing the model on a face recognition task, it seemed unable to
    reliably distinguish between people. Our hypothesis is that this is
    primarily due to the small embedding size, but also due to using
    grayscale images

--- siamese_net/model.py
+++ siamese_net/model.py
@@ -7,7 +7,7 @@ class SiameseNetwork(nn.Module):
     def __init__(self):
         super(SiameseNetwork, self).__init__()
         self.cnn1 = nn.Sequential(nn.ReflectionPad2d(1),
-                                  nn.Conv2d(1, 4, kernel_size=3),
+                                  nn.Conv2d(3, 4, kernel_size=3),
                                   nn.ReLU(inplace=True),
                                   nn.BatchNorm2d(4),
 
@@ -25,7 +25,9 @@ class SiameseNetwork(nn.Module):
                                  nn.ReLU(inplace=True),
                                  nn.Linear(500, 500),
                                  nn.ReLU(inplace=True),
-                                 nn.Linear(500, 5))
+                                 nn.Linear(500, 250),
+                                 nn.ReLU(inplace=True),
+                                 nn.Linear(250, 50))
 
     def forward_once(self, x):
         output = self.cnn1(x)
--- siamese_net/utils.py
+++ siamese_net/utils.py
@@ -8,7 +8,6 @@ def get_transforms():
 
 def get_grayscale_image_tensor(img_path):
     img = Image.open(img_path)
-    img = img.convert("L")
     img = get_transforms()(img)
     img.unsqueeze_(0)
     return img
