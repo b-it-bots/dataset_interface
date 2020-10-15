dedc4a12f0a7f745a7cc4b11d48ea791506680b6
rkutty1 <jupwy1994@gmail.com>
2020-10-15 12:39:02 +0200

    Added tensorboard functionality, issue #10

--- object_detection/engine.py
+++ object_detection/engine.py
@@ -1,5 +1,6 @@
 '''This script is a slighty modified version of the engine script provided in the
 Torchvision Object Detection Finetuning Tutorial: https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+Command to view tensorboard: tensorboard --logdir=runs then navigate to https://localhost:6006 
 '''
 import math
 import sys
@@ -8,6 +9,9 @@ import torch
 import torchvision.models.detection.mask_rcnn
 
 import dataset_interface.object_detection.utils as utils
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter()
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, loss_file_name=None):
     model.train()
@@ -50,6 +54,10 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, lo
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+    writer.add_scalar('Train_Loss/Classifier_loss',loss_dict_reduced['loss_classifier'].item(),epoch)
+    writer.add_scalar('Train_Loss/Box_reg_loss',loss_dict_reduced['loss_box_reg'].item(),epoch)
+    writer.add_scalar('Train_Loss/Objectness_loss',loss_dict_reduced['loss_objectness'].item(),epoch)
+    writer.add_scalar('Train_Loss/rpn_box_reg_loss',loss_dict_reduced['loss_rpn_box_reg'].item(),epoch)
 
 def validate_one_epoch(model, data_loader, device, epoch, print_freq, loss_file_name=None):
     model.train()
@@ -78,3 +86,7 @@ def validate_one_epoch(model, data_loader, device, epoch, print_freq, loss_file_
             sys.exit(1)
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+    writer.add_scalar('Val_Loss/Classifier_loss',loss_dict_reduced['loss_classifier'].item(),epoch)
+    writer.add_scalar('Val_Loss/Box_reg_loss',loss_dict_reduced['loss_box_reg'].item(),epoch)
+    writer.add_scalar('Val_Loss/Objectness_loss',loss_dict_reduced['loss_objectness'].item(),epoch)
+    writer.add_scalar('Val_Loss/rpn_box_reg_loss',loss_dict_reduced['loss_rpn_box_reg'].item(),epoch)
